da8ac722c06f5a34def68a9acafaf8bc11043f2f
f5-nascimento <nascimento.conceito@gmail.com>
2021-08-16 15:44:03 -0300

    Casting e Formatação de datas

--- vida.py
+++ vida.py
@@ -1,20 +1,27 @@
 #FUNÇÃO DE ENTRADA DE DADOS
-nome = input("Digite o seu nome: ")
-dataNascimento = input("Digite a sua data de nascimento: ")
-peso = float(input("Digite o seu peso: "))
-altura = float(input("Digite o sua altura: "))
-tipo = input("Digite o seu tipo sanguíneo: ")
+nome           = input("Digite o seu nome: ")
+from datetime import date, datetime
+dataNascimento = datetime.strptime(input("Digite a sua data de nascimento: "),'%d/%m/%Y').date()
+#print(dataNascimento.strftime('%d/%m/%Y'))
+peso           = float(input("Digite o seu peso: "))
+altura         = float(input("Digite o sua altura: "))
+tipo           = int(input("Escolha o seu tipo sanguíneo: " + "\n" +
+                       "1 - Tipo A"  + "\n" +
+                       "2 - Tipo B" + "\n" +
+                       "3 - Tipo AB" + "\n" +
+                       "4 - Tipo O: "))
 
 #FUNÇÃO DE SAÍDA DE DADOS
 print(nome + "\n" +
-      dataNascimento + "\n" +
+      str(dataNascimento.strftime('%d/%m/%Y')) + "\n" +
       str(peso) + "\n" +
       str(altura) + "\n" +
-      tipo)
+      str(tipo))
 
 #UTILIZANDO A FUNÇÃO TYPE
-# print(type(nome), "\n",
-#       type(dataNascimento), "\n",
-#       type(peso), "\n",
-#       type(altura), "\n",
-#       type(tipo))
+print(type(nome), "\n",
+      type(dataNascimento), "\n",
+      type(peso), "\n",
+      type(altura), "\n",
+      type(tipo))
+
